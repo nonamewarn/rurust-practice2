7232048b194f2b72287c38264b69fbdf964636a0
nonamewarn <danyakirin999@gmail.com>
2026-03-12 17:03:43 +0100

    Implement gradingStudents function for grade rounding

--- task4.rs
+++ task4.rs
@@ -1,1 +1,18 @@
+fn gradingStudents(grades: &[i32]) -> Vec<i32> {
+    grades
+        .iter()
+        .map(|&g| {
+            if g < 38 {
+                g
+            } else {
+                let remainder = g % 5;
+                if 5 - remainder < 3 {
+                    g + (5 - remainder)
+                } else {
+                    g
+                }
+            }
+        })
+        .collect()
+}
 

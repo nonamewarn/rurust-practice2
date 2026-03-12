b39e1c4128ee0025778b8bbea5356076e0afbc7c
nonamewarn <danyakirin999@gmail.com>
2026-03-12 16:41:22 +0100

    hackerrank

--- task3.rs
+++ task3.rs
@@ -1,1 +1,9 @@
+fn staircase(n: i32) {
+    for i in 1..=n {
+        let spaces = (n - i) as usize;
+        let hashes = i as usize;
+
+        println!("{}{}", " ".repeat(spaces), "#".repeat(hashes));
+    }
+}
 
